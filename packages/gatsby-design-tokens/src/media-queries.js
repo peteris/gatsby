53182f09e7a7cc348c8686fd416ec99ada54d013
Peteris Bikis <peteris@Peteriss-MacBook-Pro.local>
2020-04-08 10:54:40 +0100

    Move packages/gatsby to repo root

--- packages/gatsby-design-tokens/src/media-queries.js
+++
@@ -1,8 +0,0 @@
-import { breakpoints } from "./breakpoints"
-
-let mq = {}
-for (let breakpoint in breakpoints) {
-  mq[breakpoint] = `@media (min-width: ${breakpoints[breakpoint]})`
-}
-
-export const mediaQueries = mq
