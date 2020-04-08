814dbdf0a94b893f155b2b9bdf7b15018ad5ae44
Peteris Bikis <peteris@Peteriss-MacBook-Pro.local>
2020-04-08 11:11:20 +0100

    Fix build issues

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,6 @@
 // packages in `internal-plugins` to be compiled with the rest of the source.
 // Ref: https://github.com/babel/babel/pull/7358
 
-const configPath = require(`path`).join(__dirname, `..`, `..`, `.babelrc.js`)
+const configPath = require(`path`).join(__dirname, `.babelrc.js`)
 
 module.exports = require(configPath)
